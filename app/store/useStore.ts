d9a40a5b28f63dea569e185803b44d05f947bf78
RedMagic46 <naufalmuamar46@gmail.com>
2026-05-08 18:16:15 +0700

    ga sengaja kehapusss

--- app/store/useStore.ts
+++ app/store/useStore.ts
@@ -45,7 +45,7 @@ export const useAuthStore = create<AuthState>((set) => ({
 
   loginApi: async (email, password) => {
     try {
-      const res = await fetch('/api/login', {
+      const res = await fetch('/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
